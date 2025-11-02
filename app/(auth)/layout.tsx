e453e85704f03304cc55ee5c34a484b54b29fe24
ThanhTumg <thanhtumg.2510@gmail.com>
2025-11-02 20:11:02 +0700

    Feature: Sign in view, Sign up view

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,11 +1,17 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 interface Props {
   children: React.ReactNode;
 }
 
 export default function AuthLayout({ children }: Props) {
+  const pathName = usePathname();
+
   return (
     <div className="bg-neutral-100 min-h-screen">
       <div className="mx-auto max-w-screen-2xl p-4">
@@ -13,7 +19,11 @@ export default function AuthLayout({ children }: Props) {
           <Image src="/Logo.svg" height={56} width={152} alt="Logo" />
 
           <div className="flex items-center gap-2">
-            <Button variant={"secondary"}>Sign up</Button>
+            <Button asChild variant={"secondary"}>
+              <Link href={pathName === "/sign-in" ? "/sign-up" : "/sign-in"}>
+                {pathName === "/sign-in" ? "Sign Up" : "Login"}
+              </Link>
+            </Button>
           </div>
         </nav>
 
